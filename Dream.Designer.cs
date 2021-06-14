d9bda27b1ab2efa8cd2e1f0f257636ec6556764e
World Functions <smallkay063@outlook.com>
2021-06-14 22:24:40 +0800

    HuanDreamDiaLog

---
+++ Dream.Designer.cs
@@ -0,0 +1,322 @@
+﻿namespace HuanDreamDiaLog
+{
+    partial class Dream
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Dream));
+            this.label1 = new System.Windows.Forms.Label();
+            this.title = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.NR = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.CTC = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.onetime = new System.Windows.Forms.CheckBox();
+            this.always = new System.Windows.Forms.CheckBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.yanchi = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.stop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 12F);
+            this.label1.Location = new System.Drawing.Point(295, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 21);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "标题";
+            // 
+            // title
+            // 
+            this.title.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.title.Location = new System.Drawing.Point(299, 30);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(336, 29);
+            this.title.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("微软雅黑", 12F);
+            this.label2.Location = new System.Drawing.Point(295, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 21);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "内容";
+            // 
+            // NR
+            // 
+            this.NR.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.NR.Location = new System.Drawing.Point(299, 86);
+            this.NR.Multiline = true;
+            this.NR.Name = "NR";
+            this.NR.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.NR.Size = new System.Drawing.Size(336, 71);
+            this.NR.TabIndex = 3;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "无",
+            "意外性终止(红×)",
+            "警告(感叹号)",
+            "询问用户(问号)"});
+            this.comboBox1.Location = new System.Drawing.Point(299, 192);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(160, 28);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("微软雅黑", 12F);
+            this.label3.Location = new System.Drawing.Point(295, 170);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 21);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "图标";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("微软雅黑", 12F);
+            this.label4.Location = new System.Drawing.Point(471, 170);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 21);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "按钮";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "确定",
+            "确定+取消",
+            "重试+取消",
+            "是+否+取消",
+            "是+否",
+            "中止+重试+忽略"});
+            this.comboBox2.Location = new System.Drawing.Point(475, 192);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(160, 28);
+            this.comboBox2.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(556, 397);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(79, 41);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "创建";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // CTC
+            // 
+            this.CTC.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.CTC.Location = new System.Drawing.Point(299, 255);
+            this.CTC.MaxLength = 10;
+            this.CTC.Name = "CTC";
+            this.CTC.Size = new System.Drawing.Size(160, 29);
+            this.CTC.TabIndex = 9;
+            this.CTC.Text = "1";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("微软雅黑", 12F);
+            this.label5.Location = new System.Drawing.Point(295, 231);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(42, 21);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "数量";
+            // 
+            // onetime
+            // 
+            this.onetime.AutoSize = true;
+            this.onetime.BackColor = System.Drawing.Color.Transparent;
+            this.onetime.Checked = true;
+            this.onetime.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.onetime.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.onetime.Location = new System.Drawing.Point(480, 257);
+            this.onetime.Name = "onetime";
+            this.onetime.Size = new System.Drawing.Size(77, 25);
+            this.onetime.TabIndex = 11;
+            this.onetime.Text = "一次性";
+            this.onetime.UseVisualStyleBackColor = false;
+            this.onetime.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // always
+            // 
+            this.always.AutoSize = true;
+            this.always.BackColor = System.Drawing.Color.Transparent;
+            this.always.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.always.Location = new System.Drawing.Point(569, 257);
+            this.always.Name = "always";
+            this.always.Size = new System.Drawing.Size(61, 25);
+            this.always.TabIndex = 12;
+            this.always.Text = "耐久";
+            this.always.UseVisualStyleBackColor = false;
+            this.always.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.groupBox1.Location = new System.Drawing.Point(475, 232);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(160, 114);
+            this.groupBox1.TabIndex = 14;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "模式";
+            // 
+            // yanchi
+            // 
+            this.yanchi.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.yanchi.Location = new System.Drawing.Point(299, 317);
+            this.yanchi.MaxLength = 10;
+            this.yanchi.Name = "yanchi";
+            this.yanchi.Size = new System.Drawing.Size(160, 29);
+            this.yanchi.TabIndex = 15;
+            this.yanchi.Text = "500";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("微软雅黑", 12F);
+            this.label6.Location = new System.Drawing.Point(295, 293);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(116, 21);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "延迟(1000=1s)";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("微软雅黑", 9F);
+            this.button2.ForeColor = System.Drawing.Color.Red;
+            this.button2.Location = new System.Drawing.Point(299, 397);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(149, 41);
+            this.button2.TabIndex = 17;
+            this.button2.Text = "关闭所有窗口并退出程序";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // stop
+            // 
+            this.stop.Enabled = false;
+            this.stop.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.stop.Location = new System.Drawing.Point(454, 397);
+            this.stop.Name = "stop";
+            this.stop.Size = new System.Drawing.Size(96, 42);
+            this.stop.TabIndex = 18;
+            this.stop.Text = "终止循环";
+            this.stop.UseVisualStyleBackColor = true;
+            this.stop.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Dream
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(665, 450);
+            this.Controls.Add(this.stop);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.yanchi);
+            this.Controls.Add(this.always);
+            this.Controls.Add(this.onetime);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.CTC);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.NR);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.title);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Dream";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "HuanDreamDiaLog";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox title;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox NR;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox CTC;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.CheckBox onetime;
+        private System.Windows.Forms.CheckBox always;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox yanchi;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button stop;
+    }
+}
+
---
+++ Dream.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace HuanDreamDiaLog
+{
+    public partial class Dream : Form
+    {
+        int shuliang;
+        int yan;
+        string APPNAME = "HuanDreamDiaLog";
+        public Dream()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Control.CheckForIllegalCrossThreadCalls = false;
+            this.Text = APPNAME;
+            comboBox1.SelectedIndex = 0;
+            comboBox2.SelectedIndex = 0;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int count = int.Parse(CTC.Text);
+                if (count <= 0)
+                {
+                    MessageBox.Show("生成数量非正整数", "参数错误", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                    return;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("生成数量非正整数", "参数错误", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                return;
+            }
+            try
+            {
+                int count = int.Parse(yanchi.Text);
+                if (count <= 0)
+                {
+                    MessageBox.Show("延迟值非正整数", "参数错误", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                    return;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("延迟值非正整数", "参数错误", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                return;
+            }
+            stop.Enabled = false;
+            shuliang = int.Parse(CTC.Text);
+            yan = int.Parse(yanchi.Text);
+            
+            if (onetime.Checked)
+            {
+                Thread thread = new Thread(new ThreadStart(Start));
+                thread.Start();
+            }
+            else if (always.Checked)
+            {
+                Thread thread = new Thread(new ThreadStart(Astart));
+                thread.Start();
+            }
+        }
+
+        public void Create(string title, string mains, int type1,int type2)
+        {
+            if (type1 == 1)
+            {
+                if (type2 == 1) { MessageBox.Show(mains, title, MessageBoxButtons.OK); }
+                if (type2 == 2) { MessageBox.Show(mains, title, MessageBoxButtons.OKCancel); }
+                if (type2 == 3) { MessageBox.Show(mains, title, MessageBoxButtons.RetryCancel); }
+                if (type2 == 4) { MessageBox.Show(mains, title, MessageBoxButtons.YesNoCancel); }
+                if (type2 == 5) { MessageBox.Show(mains, title, MessageBoxButtons.YesNo); }
+                if (type2 == 6) { MessageBox.Show(mains, title, MessageBoxButtons.AbortRetryIgnore); }
+            }
+            else if (type1 == 2)
+            {
+                if (type2 == 1) { MessageBox.Show(mains, title, MessageBoxButtons.OK, MessageBoxIcon.Stop); }
+                if (type2 == 2) { MessageBox.Show(mains, title, MessageBoxButtons.OKCancel, MessageBoxIcon.Stop); }
+                if (type2 == 3) { MessageBox.Show(mains, title, MessageBoxButtons.RetryCancel, MessageBoxIcon.Stop); }
+                if (type2 == 4) { MessageBox.Show(mains, title, MessageBoxButtons.YesNoCancel, MessageBoxIcon.Stop); }
+                if (type2 == 5) { MessageBox.Show(mains, title, MessageBoxButtons.YesNo, MessageBoxIcon.Stop); }
+                if (type2 == 6) { MessageBox.Show(mains, title, MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Stop); }
+            }
+            else if (type1 == 3)
+            {
+                if (type2 == 1) { MessageBox.Show(mains, title, MessageBoxButtons.OK, MessageBoxIcon.Warning); }
+                if (type2 == 2) { MessageBox.Show(mains, title, MessageBoxButtons.OKCancel, MessageBoxIcon.Warning); }
+                if (type2 == 3) { MessageBox.Show(mains, title, MessageBoxButtons.RetryCancel, MessageBoxIcon.Warning); }
+                if (type2 == 4) { MessageBox.Show(mains, title, MessageBoxButtons.YesNoCancel, MessageBoxIcon.Warning); }
+                if (type2 == 5) { MessageBox.Show(mains, title, MessageBoxButtons.YesNo, MessageBoxIcon.Warning); }
+                if (type2 == 6) { MessageBox.Show(mains, title, MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Warning); }
+            }
+            else if (type1 == 4)
+            {
+                if (type2 == 1) { MessageBox.Show(mains, title, MessageBoxButtons.OK, MessageBoxIcon.Question); }
+                if (type2 == 2) { MessageBox.Show(mains, title, MessageBoxButtons.OKCancel, MessageBoxIcon.Question); }
+                if (type2 == 3) { MessageBox.Show(mains, title, MessageBoxButtons.RetryCancel, MessageBoxIcon.Question); }
+                if (type2 == 4) { MessageBox.Show(mains, title, MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question); }
+                if (type2 == 5) { MessageBox.Show(mains, title, MessageBoxButtons.YesNo, MessageBoxIcon.Question); }
+                if (type2 == 6) { MessageBox.Show(mains, title, MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Question); }
+            }
+        }
+        public void Start()
+        {
+            this.Text = APPNAME + "：" + "生成" + title.Text + "-" + NR.Text + "*" + shuliang.ToString();
+            stop.Enabled = true;
+            onetime.Enabled = false;
+            always.Enabled = false;
+            while (shuliang > 0)
+            {
+                System.Threading.Thread.Sleep(yan);
+                Thread thread = new Thread(new ThreadStart(Thing));
+                thread.Start();
+                shuliang--;
+            }
+            stop.Enabled = false;
+            onetime.Enabled = true;
+            always.Enabled = true;
+            this.Text = APPNAME;
+        }
+        public void Thing()
+        {
+            Create(title.Text, NR.Text, Cicon(comboBox1), Cbt(comboBox2));
+        }
+        public void Astart()
+        {
+            this.Text = APPNAME + "：" + "生成" + title.Text + "-" + NR.Text + "*" + shuliang.ToString();
+            stop.Enabled = true;
+            onetime.Enabled = false;
+            always.Enabled = false;
+            while (shuliang > 0)
+            {
+                Create(title.Text, NR.Text, Cicon(comboBox1), Cbt(comboBox2));
+                shuliang--;
+            }
+            stop.Enabled = false;
+            onetime.Enabled = true;
+            always.Enabled = true;
+            this.Text = APPNAME;
+        }
+
+        public int Cicon(ComboBox main)
+        {
+            if (main.Text == "无") { return 1; }
+            else if (main.Text == "意外性终止(红×)") { return 2; }
+            else if (main.Text == "警告(感叹号)") { return 3; }
+            else if (main.Text == "询问用户(问号)") { return 4; }
+            else { return 1; }
+        }
+        public int Cbt(ComboBox main)
+        {
+            if (main.Text == "确定") { return 1; }
+            else if (main.Text == "确定+取消") { return 2; }
+            else if (main.Text == "重试+取消") { return 3; }
+            else if (main.Text == "是+否+取消") { return 4; }
+            else if (main.Text == "是+否") { return 5; }
+            else if (main.Text == "中止+重试+忽略") { return 6; }
+            else { return 1; }
+        }
+
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (onetime.Checked)
+            {
+                always.Checked = false;
+            }
+            if (onetime.Checked==false&&always.Checked==false)
+            {
+                onetime.Checked = true;
+            }
+        }
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (always.Checked)
+            {
+                onetime.Checked = false;
+            }
+            if (onetime.Checked == false && always.Checked == false)
+            {
+                always.Checked = true;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            shuliang = 0;
+            stop.Enabled = false;
+            onetime.Enabled = true;
+            always.Enabled = true;
+            this.Text = APPNAME;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            System.Environment.Exit(0);
+        }
+    }
+}
